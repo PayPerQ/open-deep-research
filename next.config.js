56d331cf4910c7879fead04dda6fb2f0a6ea8deb
mattius459 <matt.ahlborg@gmail.com>
2025-02-24 13:57:21 +0100

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     basePath: '/deepresearch',
+  
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [{ key: 'x-forwarded-host', value: 'ppq.ai' }],
+        },
+      ];
+    },
   };
   
   module.exports = nextConfig;
+  
