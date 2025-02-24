d4c07666bf7875e3a01330f883db4608f3863fe7
mattius459 <matt.ahlborg@gmail.com>
2025-02-24 14:00:00 +0100

    lets go back to previous state

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    basePath: '/deepresearch',
-  
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [{ key: 'x-forwarded-host', value: 'ppq.ai' }],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
