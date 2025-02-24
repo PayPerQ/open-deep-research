eb009875040a5c2e2f88b6a6bf06ffd04c6af523
mattius459 <matt.ahlborg@gmail.com>
2025-02-24 13:30:00 +0100

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+    reactStrictMode: true,
+    basePath: '/deepresearch', // <-- This line added
+  };
+  
+  module.exports = nextConfig;
+  
