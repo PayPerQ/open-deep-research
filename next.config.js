d556a98d20119ff9b5e2ed53288d6123fa61d717
mattius459 <matt.ahlborg@gmail.com>
2025-02-25 14:33:40 +0100

    asdf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Removing basePath to fix proxy issue
-  // basePath: '/deepresearch',
+  basePath: '/deepresearch',
   reactStrictMode: true,
 };
 
