f44c018bc64fe8b84e8b9945e5f50d16bda68d44
mattius459 <matt.ahlborg@gmail.com>
2025-02-26 15:04:33 +0100

    asdf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/deepresearch'
+  assetPrefix: '/deepresearch',
+  basePath: '/deepresearch',
+
 };
 
 module.exports = nextConfig;
