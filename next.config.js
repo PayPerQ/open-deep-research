e81b3200ec4b05ae6ebbc450ee9a245bbf65ab2e
mattius459 <matt.ahlborg@gmail.com>
2025-02-28 11:13:49 +0100

    fixed local

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: 'https://deepresearch.ppq.ai'
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://deepresearch.ppq.ai' : undefined
 
 };
 
